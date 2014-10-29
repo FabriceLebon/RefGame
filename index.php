6d9747d897bc92630f6d247039aaee913bcbcdea
FabriceLebon <fabrice.lebon@outlook.com>
2014-10-29 21:22:27 +0100

    test acces BDD

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-phpinfo();
+try {
+  $conn = new PDO ( "sqlsrv:server = tcp:h8lwqxb5tn.database.windows.net,1433; Database = Game", "game_read_u", "ReaAzu13");
+  $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch ( PDOException $e ) {
+   print( "Error connecting to SQL Server." );
+   die(print_r($e));
+}
+
+$reponse = $bdd->query('SELECT * FROM Jeux');
+
+while ($donnees = $reponse->fetch())
+{
+?>
+    <p>
+    <strong>Jeu</strong> : <?php echo $donnees['NameGame']; ?><br />
+    L'année de ce jeu est : <?php echo $donnees['YearGame']; ?>, et le PEGI est  <?php echo $donnees['PEGI']; ?> <br />
+   </p>
+<?php
+}
+$reponse->closeCursor(); // Termine le traitement de la requête
 ?>
