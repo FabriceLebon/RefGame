fccb6513cecf5865f21245e46f9ad469e82c1948
FabriceLebon <fabrice.lebon@outlook.com>
2015-02-28 12:44:25 +0100

    Problème technique résolu

--- index.php
+++ index.php
@@ -1,36 +1,23 @@
 <html>
 <head>
-  <title>Gestion Jeux</title>
+    <title>Gestion Jeux</title>
 </head>
 <body>
 
 <?php
-echo "<br>Tentative de connexion<br>";
-try{
-  $conn = new PDO( "sqlsrv:Server = tcp:h8lwqxb5tn.database.windows.net,1433; Database = Game", "game_read@h8lwqxb5tn", "ReaAzu13");
-  $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch ( PDOException $e ) {
-   print( "Error connecting to SQL Server." );
-   die(print_r($e));
-}
+    include_once 'lib/base.php';
 
-echo "<br>Connexion reussie<br>";
-$sql = "SELECT * FROM Jeux";
-$reponse = $conn->query($sql);
-var_dump($reponse);
-echo "<br>Resultat requete stockee<br>";
-while ($donnees = $reponse->fetch())
-{
-?>
-    <p>
-    <strong>Jeu</strong> : <?php echo $donnees['NameGame']; ?><br />
-    L'année de ce jeu est : <?php echo $donnees['YearGame']; ?>, et le PEGI est  <?php echo $donnees['PEGI']; ?> <br />
-   </p>
-<?php
-}
-echo "<br>Resultat requete affichee<br>";
-$reponse->closeCursor(); // Termine le traitement de la requête
+    $conn = connect();
+    $jeux = listeTousLesjeux($conn);
+
+    foreach ($jeux as $jeu)
+    {
+        echo "<p>
+        <strong>Jeu</strong> : ".$jeu['NameGame']."<br />
+        L'année de ce jeu est : ".$jeu['YearGame'].", et le PEGI est  ".$jeu['PEGI']." <br />
+       </p>";
+    }
+
 ?>
 
 </body>
---
+++ lib/base.php
@@ -0,0 +1,29 @@
+<?php
+
+function connect()
+{
+    $host = "h8lwqxb5tn.database.windows.net,1433";
+    $user = "game_read@h8lwqxb5tn";
+    $pwd = "ReaAzu13";
+    $db = "Game";
+
+    try{
+        $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(PDOException $e){
+        print( "Error connecting to SQL Server." );
+        die(print_r($e));
+    }
+    return $conn;
+}
+
+function listeTousLesJeux($conn)
+{
+//    $conn = connect();
+    $sql = "SELECT NameGame, YearGame, PEGI FROM Jeux ORDER BY NameGame";
+    $stmt = $conn->query($sql);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+?>
