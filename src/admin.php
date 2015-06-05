4a720c09b86a2ddf5ed657fd68641e1c4f159479
FabriceLebon <FabriceLebon@users.noreply.github.com>
2015-06-05 14:14:53 +0200

    ajout verif authen et menu

--- src/admin.php
+++ src/admin.php
@@ -1,1 +1,24 @@
-
+ <?php session_start(); ?>
+ 
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Gestion Jeux - admin</title>
+  </head>
+<body>
+<?php  if (!user_verified()) { ?>
+  <div id="connexion">
+    <form action="" method="post">
+    Veuillez saisir votre login et mot de passe :<br/>
+    <input type="text" name="login" placeholder="Pseudo:" /><br />
+    <input type="text" name="pwd" placeholder="mot de passe:" /><br/>
+    <input type="submit" value="Go" />
+    </form>
+  </div>
+<?php
+} else {
+  include("src/menu.php");
+}
+?>
+</body>
+</html>
